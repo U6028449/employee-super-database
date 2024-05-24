b2ae2fd039afb277924a7d5176290fcb5005f53c
U6028449 <160793922+U6028449@users.noreply.github.com>
2024-05-23 20:22:41 -0600

    added assets

--- src/utils/test.js
+++
@@ -1,9 +0,0 @@
-const inquirer = require('inquirer');
-
-inquirer.prompt({
-    type: 'input',
-    name: 'test',
-    message: 'This is a test',
-}).then(answers => {
-    console.log(answers);
-});
